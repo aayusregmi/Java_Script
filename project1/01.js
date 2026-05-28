8d42cd4d440b027b32bbe11c4e32c282d97d0b86
Aayus Regmi <aayusregmi721@gmail.com>
2026-05-29 00:08:53 +0545

    practice Question

---
+++ project1/01.js
@@ -0,0 +1,19 @@
+//first project
+const product = {
+    Name : "Parker Jotter Standard CT Ball Pen",
+    rating : 4,
+    color : "Black",
+    price : 285,
+    Discount_price : 270,
+};
+console.log(product);
+//second project
+const profile = {
+    fullName : "Sharadha Khapra",
+    isfollow : false,
+    post : 195,
+    followers : "569k",
+    following : 4,
+    profession : "Entreprenur"
+};
+console.log(profile);
