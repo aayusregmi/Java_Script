bf57c5ef4ba05499b48a45f7ab2c45db23a5b3a1
Aayus Regmi <aayusregmi721@gmail.com>
2026-05-31 19:53:59 +0545

    Array Method

--- array/array.js
+++ array/array.js
@@ -19,3 +19,36 @@ let cities = ["Baglung" , "pokhara" , "Kathmandau"];
 for(let city of cities){
     console.log(city.toUpperCase());
 }
+
+console.log("<-----array method---->");
+let vegetable = ["Potato" , "Cabbage" , "Cauliflower" , "Brocauli" , "Beans"];
+console.log(vegetable);
+vegetable.push("Cucumber");
+console.log(vegetable);
+let deletedItem = vegetable.pop();
+console.log(vegetable);
+console.log(deletedItem);
+
+let fruit = ["Apple" , "Banana" , "Mango" , "Litchi" , "Grape"];
+let chocolate = ["Dark Chocolate" , "Dark Fantasy"];
+console.log(fruit);
+console.log(fruit.toString());
+let foodItems = vegetable.concat(fruit , chocolate);
+console.log(foodItems);
+
+chocolate.unshift("Kitkat");
+console.log(chocolate);
+
+let deletedChoco = chocolate.shift();
+console.log(chocolate);
+console.log("deleted " , deletedChoco);
+
+console.log(foodItems.slice(2 , 5));
+
+let arr = [1 , 2 , 3 , 4 , 5 , 6];
+//add
+// arr.splice(2 , 0 , 2.5);
+//delete
+//arr.splice(2 , 1);
+//replace
+arr.splice(2 , 1 , 3.5);
