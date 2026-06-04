583b7bca0acc7a62e478f43b5fe01f993d6de751
Aayus Regmi <aayusregmi721@gmail.com>
2026-06-04 11:36:54 +0545

    Question Practice on DOM properties and accesing nodes

--- Document_objectmodel/DOM.js
+++ Document_objectmodel/DOM.js
@@ -15,3 +15,5 @@ let ele1 = document.querySelector(".myPara"); //return first element of related 
 console.dir(ele1);
 let ele2 = document.querySelectorAll(".myPara"); //return node list of related class
 console.dir(ele2);
+//first child : there are nodes as the child such as--> text , comment and element so if i need specifically the element child then i have to use firstElementChild
+console.log(document.getElementById("para-01").firstElementChild);
---
+++ Document_objectmodel/firstQNDOM.js
@@ -0,0 +1,18 @@
+//Qs. Create a H2 heading element with text - “Hello JavaScript”. Append “from Apna College students” to this text using JS.
+let h2Addition = document.querySelector("h2");
+h2Addition.innerText = h2Addition.innerText +" " + "From Aapna College Student";
+
+
+//Qs.Create 3 divs with common class name - “box”. Access them & add some unique text to each of them.
+let div = document.querySelectorAll(".box");
+div[0].innerText = "First box";
+div[1].innerText = "Second box";
+div[2].innerText = "third box";
+
+
+
+
+
+
+
+
