059ff2aa7f9f045200b74cd9f10f1525d6925a13
Aayus Regmi <aayusregmi721@gmail.com>
2026-06-05 08:34:47 +0545

    removechild- appendchild

---
+++ Document_objectmodel/childParentNode.js
@@ -0,0 +1,10 @@
+//appendChild
+const lastDesert = document.getElementById("desert-list");
+const endChild = document.createElement("li");
+endChild.innerText = "Cookie";
+lastDesert.appendChild(endChild);
+
+//removeChild
+const scnDiv = document.getElementById("second-div");
+const rmvChild = document.querySelector("#second-div p:last-of-type");
+scnDiv.removeChild(rmvChild);
