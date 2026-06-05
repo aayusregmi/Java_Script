51b020aaf381b7f3ded6f7974438d88632d9b0b4
Aayus Regmi <aayusregmi721@gmail.com>
2026-06-05 21:08:53 +0545

    Question practice - changing the background color with the help of DOM

---
+++ Events!/PracticeQuestion.js
@@ -0,0 +1,23 @@
+const btn = document.querySelector("#button-mode");
+const bkground = document.querySelector("body");
+let currentMode_body = "Light";
+let currentMode_button = "Black"
+let currentBtnColor = "Light"
+btn.addEventListener("click" , ()=>{
+    if(currentMode_body === "Light"){
+        bkground.style.backgroundColor = "black";
+        currentMode_body = "Dark";
+        btn.style.backgroundColor = "white";
+        currentMode_button = "Light"
+        btn.style.color = "black";
+        currentBtnColor = "black";
+      
+    }else{
+      bkground.style.backgroundColor = "white";
+      currentMode_body = "Light";
+      btn.style.backgroundColor = "black";
+      currentMode_button = "Black";
+      btn.style.color = "white";
+      currentBtnColor = "white";
+    }
+})
