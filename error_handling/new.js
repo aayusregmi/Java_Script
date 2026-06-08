18eb05484370bbc46ad190fcc6ad1394e3147fc6
Aayus Regmi <aayusregmi721@gmail.com>
2026-06-08 20:52:52 +0545

    error handling

---
+++ error_handling/new.js
@@ -0,0 +1,12 @@
+const a = 19;
+const b = 20;
+console.log(` a+b = ${a + b}`);
+console.log(` a+b = ${a + b}`);
+try{
+console.log(` a+b = ${a + c}`);
+}catch(err){
+    console.log(err);
+}
+console.log(` a+b = ${a + b}`);
+console.log(` a+b = ${a + b}`);
+
